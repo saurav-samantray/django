b0b33e65e95ad2bad7b36644a304c5224a70b85a
Saurav Samantray <ssamantray@walmart.com>
2019-12-06 14:01:55 +0530

    cart component update

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -7,20 +7,26 @@ from .models import Cart
 
 def cart_home(request):
 	cart_obj,new_obj = Cart.objects.new_or_get(request)
+
 	cart_id = cart_obj.id
-
 	subtotal = cart_obj.subtotal
+	session_key = request.session.session_key
+	print(f'GET_CART: sessionKey={session_key} cartId={cart_id} , subTotal={subtotal} ')
 
-	print(f'GET_CART:  cartId={cart_id} , subTotal={subtotal} ')
-
-	print("Session Key : ",request.session.session_key)
-	return render(request, "carts/home.html", {})
+	return render(request, "carts/home.html", {"cart":cart_obj})
 
 def cart_update(request):
-	product_id = 1
-	product_obj = Product.objects.get(id=product_id)
-	cart_obj,new_obj = Cart.objects.new_or_get(request)
-	cart_obj.products.add(product_obj)
-	#cart_obj.products.remove(product_obj)
-	#return redirect(product_obj.get_absolute_url())
+	product_id = request.POST.get("product_id")
+	if product_id is not None:
+		try:
+			product_obj = Product.objects.get(id=product_id)
+		except Product.DoesNotExist:
+			print("Show message to user: Product doesn't exist")
+			return redirect("cart:home")
+		cart_obj,new_obj = Cart.objects.new_or_get(request)
+		if product_obj in cart_obj.products.all():
+			cart_obj.products.remove(product_obj)
+		else:
+			cart_obj.products.add(product_obj)
+		request.session['cart_items'] = cart_obj.products.count() 
 	return redirect("cart:home")
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -81,12 +81,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {   ‘default’: {      ‘ENGINE’: ‘djongo’,      ‘NAME’: ‘your-db-name’,   }}
 
 
 # Password validation
